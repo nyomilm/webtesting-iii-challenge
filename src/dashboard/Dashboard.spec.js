d9b45cdb2748af14e62b66c760bd190b5f4fec59
nyomilm <nyomilm@hotmail.com>
2019-10-06 21:07:39 -0500

    3rd update controls, dashboard done, display in progress

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,21 @@
+import React from 'react';
+import Dashboard from './Dashboard';
+import { render } from '@testing-library/react';
+
+
+test("component renders", () => {
+    render(<Dashboard />);
+  });
+
+  test('Dashboard renders with gate', () => {
+      const { getByText } = render(<Dashboard />);
+
+      //dahsboard renders with gate
+      getByText(/unlocked/i);
+      getByText(/lock gate/i);
+      getByText(/open/i);
+      getByText(/closed gate/i);
+
+      
+  });
 // Test away
